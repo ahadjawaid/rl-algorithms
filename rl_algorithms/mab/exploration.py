3263387fd566cb1281e8a7bad7ade30fee656789
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-07-23 20:00:12 -0700

    feat: added EpsilonGreedyLinearDecay strategy

--- rl_algorithms/mab/exploration.py
+++ rl_algorithms/mab/exploration.py
@@ -2,12 +2,10 @@ import numpy as np
 from tqdm.auto import tqdm
 import gym
 import multi_armed_bandits
-from typing import Callable
-from functools import partial
 from gym import Env
 
 class Strategy:
-    def __init__(self, env: Env, n_episodes: int = 5000) -> None:
+    def __init__(self, env: Env, n_episodes: int = 5000, *args, **kwargs) -> None:
         self.env = env
         self.n_episodes = n_episodes
         self.n_actions = env.action_space.n
@@ -15,13 +13,13 @@ class Strategy:
 
         self.Q, self.N = np.zeros((self.n_actions)), np.zeros((self.n_actions))
 
-    def _action_selection(self):
+    def _action_selection(self, *args, **kwargs):
         raise Exception("Not Implemented _action_selection")
     
     def run(self, display=True, *args, **kwargs):
         self.env.reset()
 
-        rewards = np.zeros((self.n_episodes, self.n_actions))
+        rewards = np.zeros((self.n_episodes))
 
         for e in tqdm(range(self.n_episodes), desc=f"{self.name} episodes", leave=True, disable=(not display)):
             action = self._action_selection(*args, **kwargs)
@@ -41,30 +39,56 @@ class Strategy:
         np.random.seed(seed)
         env = gym.make(f"{n_arms}-Armed-Bandit")
 
-        strategy = cls(env=env)
+        strategy = cls(*args, env=env, **kwargs)
         Q, rewards = strategy.run(display_progress, *args, **kwargs)
 
         true_actions = np.array(list(map(lambda bandit: bandit.mean, env.unwrapped.bandits)))
 
         print(f"True action-value function: {true_actions}")
         print(f"Predicted Action-value function: {Q}")
-        print(f"Total Rewards: {round(np.sum(rewards), rounding_value)}")
+        print(f"Average episode reward: {round(np.sum(rewards) / strategy.n_episodes, rounding_value)}")
+
 
 
 class PureExploitation(Strategy):
-    def _action_selection(self):
+    def _action_selection(self, *args, **kwargs):
         return np.argmax(self.Q)
     
 
 class PureExploration(Strategy):
-    def _action_selection(self):
+    def _action_selection(self, *args, **kwargs):
         return np.random.randint(self.n_actions)
     
     
 class EpsilonGreedy(Strategy):
-    def _action_selection(self, epsilon):
+    def _action_selection(self, epsilon, *args, **kwargs):
         action = np.argmax(self.Q)
         if np.random.random() > epsilon:
             action = np.random.randint(self.n_actions)
 
         return action
+
+
+class EpsilonGreedyLinearDecay(Strategy):
+    def __init__(self, env: Env, n_episodes: int = 5000, init_epsilon=1.0, min_epsilon=0.05, decay_ratio=0.05) -> None:
+        super().__init__(env, n_episodes)
+
+        self.decay_steps = iter(linear_decay(self.n_episodes, decay_ratio, init_epsilon, min_epsilon))
+
+    def _action_selection(self, *args, **kwargs):
+        action = np.argmax(self.Q)
+        if np.random.random() > next(self.decay_steps):
+            action = np.random.randint(self.n_actions)
+
+        return action
+
+def linear_decay(n_episodes, decay_ratio, init_epsilon, min_epsilon):
+    decay_episodes = int(n_episodes * decay_ratio)
+
+    epsilon = init_epsilon
+    decay_step = (init_epsilon - min_epsilon) / decay_episodes
+    for e in range(n_episodes):
+        if e < decay_episodes:
+            epsilon -= decay_step
+
+        yield epsilon
